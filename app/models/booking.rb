0184829d615376289f866bbd1f5ff93371803177
NotGrm <notgrm@gmail.com>
2017-07-02 20:16:58 +0200

    Set booking's price before saving

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -16,10 +16,21 @@ class Booking < ApplicationRecord
   validate :end_at_is_greater_than_start_at
   validate :rental_is_available
 
+  before_save :calculate_price
+
   scope :start_before, ->(date) { where('bookings.start_at < ?', date) }
   scope :end_after, ->(date) { where('bookings.end_at > ?', date) }
 
+  def stay_duration
+    (end_at - start_at).to_i unless end_at.nil? || start_at.nil?
+  end
+
   private
+
+  def calculate_price
+    return unless stay_duration
+    self.price = rental.daily_rate * stay_duration
+  end
 
   def rental_is_available
     errors.add(:rental, 'is not available') unless rental.available?(start_at, end_at)
