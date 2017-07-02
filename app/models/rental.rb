13a5cc2d47142e84cbcee06a823b25c7bf280e5b
NotGrm <notgrm@gmail.com>
2017-07-02 19:26:23 +0200

    Add rental availibilty validation to booking

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,6 +4,12 @@
 # - name
 # - daily_rate
 class Rental < ApplicationRecord
+  has_many :bookings
+
   validates :name, presence: true, uniqueness: true
   validates :daily_rate, presence: true, numericality: { greater_than: 0 }
+
+  def available?(period_start, period_end)
+    bookings.end_after(period_start).start_before(period_end).empty?
+  end
 end
--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -13,4 +13,35 @@ RSpec.describe Rental, type: :model do
     it { is_expected.to validate_presence_of(:daily_rate) }
     it { is_expected.to validate_numericality_of(:daily_rate).is_greater_than(0) }
   end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:bookings) }
+  end
+
+  describe 'methods' do
+    describe '#available?' do
+      subject(:rental) do
+        create(:rental) do |rental|
+          create(:booking, rental: rental, start_at: '2017-06-25', end_at: '2017-06-30')
+          create(:booking, rental: rental, start_at: '2017-07-08', end_at: '2017-07-10')
+        end
+      end
+
+      it 'return true when period starts after booking ends and ends before other booking starts' do
+        expect(rental.available?('2017-06-30', '2017-07-08')).to be_truthy
+      end
+
+      it 'returns false when period match booking period' do
+        expect(rental.available?('2017-06-25', '2017-06-30')).to be_falsy
+      end
+
+      it 'returns false when period starts before booking ends' do
+        expect(rental.available?('2017-06-29', '2017-07-02')).to be_falsy
+      end
+
+      it 'returns false when period ends after booking starts' do
+        expect(rental.available?('2017-07-01', '2017-07-09')).to be_falsy
+      end
+    end
+  end
 end
