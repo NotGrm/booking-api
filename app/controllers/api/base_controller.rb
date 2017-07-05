ee2645c71ef57d4acd0afe5a648d7d4fdc8f366c
NotGrm <notgrm@gmail.com>
2017-07-06 01:07:51 +0200

    Load authentication token from rails config file

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,10 +1,13 @@
+#
 class Api::BaseController < JSONAPI::ResourceController
   before_action :require_token
 
   private
 
   def require_token
-    return if request.headers.include?('X_Auth_Token') && request.headers['X_Auth_Token'] == 'e0ed10f8'
+    auth_token = Rails.configuration.authentication_token
+
+    return if request.headers.include?('X_Auth_Token') && request.headers['X_Auth_Token'] == auth_token
     render json: { 'message': 'unauthenticated' }, status: 401
   end
 end
